8cfbf81047f821b89323595d80556414eaae31d9
Teresa Gao <t_gao@mit.edu>
2020-05-01 10:21:03 -0600

    update data extraction

--- analysis/data_extraction.R
+++ analysis/data_extraction.R
@@ -1,18 +1,25 @@
-library("rjson")
+# SETUP: Go to Session > Set Working Directory > To Source File Location
+
 library("RJSONIO")
 library("jsonlite")
 library("tidyverse")
-library("geojsonR")
 
+# Extract all file names in the MTurk sandbox-results directory
 folder_path = "../mturk/sandbox-results/"
 files <- dir(folder_path)
-getwd()
 
-json_read <- function(f){
-  open_file = fromJSON(paste(folder_path, f, sep=""))
-  return(open_file$answers$trials_data)
+# Extract trials_data from each of the MTurk files
+combined_df <- data.frame()
+for(file_name in files) {
+  full_file_name <- paste(path, file_name, sep="/")
+  test_file <- fromJSON(full_file_name)
+  file_trials_data <- test_file$answers$trials_data
+  combined_df <- rbind(combined_df, file_trials_data)
 }
 
-getwd()
-json_extracted <- map(files, json_read)
-write_json(toJSON(json_extracted), "json_extracted.json")
+# Exclude the intro "trial"
+combined_df = combined_df %>% filter(trial_num != 0)
+combined_df = subset(combined_df, select = -c(intro_time_in_seconds))
+
+# Write data to CSV file, for use in data_analysis.Rmd
+write.csv(combined_df, "combined_trials_data.csv")
