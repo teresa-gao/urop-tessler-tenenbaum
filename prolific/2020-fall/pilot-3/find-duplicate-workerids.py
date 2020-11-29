7910b303a951c515d946ccc3dd018d9986e300be
Teresa Gao <t_gao@mit.edu>
2020-11-29 09:36:20 -0700

    add non-personally identifiable data

---
+++ prolific/2020-fall/pilot-3/find-duplicate-workerids.py
@@ -0,0 +1,23 @@
+import csv
+
+pilot_2_ids = []
+pilot_3_ids = []
+
+with open("pilot-2/pilot-1-acc1-ped1-workerids.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count = 0
+    for row in csv_reader:
+        if line_count != 0:
+            pilot_2_ids.append(row[1])
+        line_count += 1
+
+with open("genex-pilot-3-workerids.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count = 0
+    for row in csv_reader:
+        if line_count != 0:
+            pilot_3_ids.append(row[1])
+        line_count += 1
+
+duplicate_ids = [ workerid for workerid in pilot_3_ids if workerid in pilot_2_ids ]
+print(duplicate_ids)
